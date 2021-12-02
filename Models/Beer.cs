a04b561598c6c947713b22dafd791b3e3406d99f
Sean Beagle <seanbeagle@gmail.com>
2021-12-02 17:38:14 -0500

    removed extra stuff

--- Models/Beer.cs
+++ Models/Beer.cs
@@ -21,15 +21,16 @@ namespace app.Models
             "Heat-Induced",
             "Biological" 
         };
-        public int    ID              { get; set; }
-        public string Brewer          { get; set; }
-        public string Name            { get; set; }
-	    public string Color           { get; set; }
-	    public string Clarity         { get; set; }
-        public string Type            { get; set; }
-	    public double AlcoholByVolume { get; set; }
-	    public double AlcoholByWeight { get; set; }
-        public int    Calories        { get; set; }
-	    public string Aroma           { get; set; }
+        public int    ID       { get; set; }
+        public string Brewer   { get; set; }
+        public string Name     { get; set; }
+	    public string Color    { get; set; }
+	    public string Clarity  { get; set; }
+        public string Type     { get; set; }
+	    public double ABV      { get; set; }
+	    public double ABW      { get; set; }
+        public double IBU      {get; set;}
+        public int    Calories { get; set; }
+	    public string Aroma    { get; set; }
     }
 }
